c76a5a8765430599e4215398eb47f46356cfde96
Alexander Likhachev <likhachev96@gmail.com>
2017-02-24 23:58:35 +0700

    Added status tips for actions

--- Life/mainwindow.cpp
+++ Life/mainwindow.cpp
@@ -28,26 +28,31 @@ void MainWindow::createActions() {
     newAct->setShortcut(tr("Ctrl+N"));
     const QIcon newIcon = QIcon::fromTheme("board-new", QIcon(":/icons/new.png"));
     newAct->setIcon(newIcon);
+    newAct->setStatusTip("Create a new Convey's Life game board");
     toolBar->addAction(newAct);
 
     QAction *openAct = fileMenu->addAction(tr("&Open..."), this, &QWidget::close);
     openAct->setShortcut(tr("Ctrl+O"));
     const QIcon openIcon = QIcon::fromTheme("board-open", QIcon(":/icons/open.png"));
     openAct->setIcon(openIcon);
+    openAct->setStatusTip("Load Convey's Life game board from file");
     toolBar->addAction(openAct);
 
     QAction *saveAct = fileMenu->addAction(tr("&Save"), this, &QWidget::close);
     saveAct->setShortcut(tr("Ctrl+S"));
     const QIcon saveIcon = QIcon::fromTheme("board-save", QIcon(":/icons/save.png"));
     saveAct->setIcon(saveIcon);
+    saveAct->setStatusTip("Save current game board to the file");
     toolBar->addAction(saveAct);
 
     QAction *saveAsAct = fileMenu->addAction(tr("&Save as..."), this, &QWidget::close);
+    saveAsAct->setStatusTip("Save current game board to specific file");
     saveAsAct->setShortcut(tr("Ctrl+Shift+S"));
 
     fileMenu->addSeparator();
 
     QAction *exitAct = fileMenu->addAction(tr("&Exit..."), this, &QWidget::close);
+    saveAsAct->setStatusTip("Exit the game");
     exitAct->setShortcut(tr("Ctrl+Q"));
 
     toolBar->addSeparator();
@@ -57,11 +62,13 @@ void MainWindow::createActions() {
     QAction *xorModeAct = editMenu->addAction(tr("&XOR mode"), this, &QWidget::close);
     xorModeAct->setCheckable(true);
     xorModeAct->setChecked(true);
+    xorModeAct->setStatusTip("Switch XOR/Replace mode");
 
     QAction *replaceModeAct = editMenu->addAction(tr("&Replace mode"), this, &QWidget::close);
     replaceModeAct->setCheckable(true);
     const QIcon replaceIcon = QIcon::fromTheme("replace-mode-switch", QIcon(":/icons/replace.png"));
     replaceModeAct->setIcon(replaceIcon);
+    replaceModeAct->setStatusTip("Switch XOR/Replace mode");
     toolBar->addAction(replaceModeAct);
 
     editMenu->addSeparator();
@@ -71,6 +78,7 @@ void MainWindow::createActions() {
     clearBoardAct->setShortcut(tr("Ctrl+L"));
     const QIcon clearBoardIcon = QIcon::fromTheme("clear-board", QIcon(":/icons/clear.png"));
     clearBoardAct->setIcon(clearBoardIcon);
+    clearBoardAct->setStatusTip("Clear the game board");
     toolBar->addAction(clearBoardAct);
 
     editMenu->addSeparator();
@@ -78,6 +86,7 @@ void MainWindow::createActions() {
     QAction *parametersAct = editMenu->addAction(tr("&Parameters"), this, &QWidget::close);
     const QIcon settingsIcon = QIcon::fromTheme("settings", QIcon(":/icons/settings.png"));
     parametersAct->setIcon(settingsIcon);
+    parametersAct->setStatusTip("Show game settings window");
     toolBar->addAction(parametersAct);
 
     toolBar->addSeparator();
@@ -86,9 +95,11 @@ void MainWindow::createActions() {
     toolBarStateAction = viewMenu->addAction(tr("&Toolbar"), this, toggleToolBar);
     toolBarStateAction->setCheckable(true);
     toolBarStateAction->setChecked(true);
+    toolBarStateAction->setStatusTip("Show/hide toolbar");
     statusBarStateAction = viewMenu->addAction(tr("&Statusbar"), this, toggleStatusBar);
     statusBarStateAction->setCheckable(true);
     statusBarStateAction->setChecked(true);
+    statusBarStateAction->setStatusTip("Show/hide statusbar");
     statusBar()->show();
 
     QMenu *simulationMenu = menuBar()->addMenu(tr("&Simulation"));
@@ -96,16 +107,19 @@ void MainWindow::createActions() {
     QAction *runOnceAct = simulationMenu->addAction(tr("&Run once"), this, &QWidget::close);
     const QIcon runOnceIcon = QIcon::fromTheme("run-once", QIcon(":/icons/run-once.png"));
     runOnceAct->setIcon(runOnceIcon);
+    runOnceAct->setStatusTip("Run game loop once");
     toolBar->addAction(runOnceAct);
 
     QAction *runLoopAct = simulationMenu->addAction(tr("&Run loop"), this, &QWidget::close);
     const QIcon runLoopIcon = QIcon::fromTheme("run", QIcon(":/icons/run.png"));
     runLoopAct->setIcon(runLoopIcon);
+    runLoopAct->setStatusTip("Run game loop");
     toolBar->addAction(runLoopAct);
 
     QAction *helpAct = menuBar()->addAction(tr("&?"), this, SLOT(showAbout()));
     helpAct->setShortcut(tr("F1"));
     const QIcon helpIcon = QIcon::fromTheme("about", QIcon(":/icons/about.png"));
+    helpAct->setStatusTip("About this application");
     helpAct->setIcon(helpIcon);
 }
 
