457d2d7e29ab3ff48fbc5fc5d68d10f7d2625d97
Alexander Likhachev <likhachev96@gmail.com>
2017-04-04 23:17:10 +0700

    [Isolines] Added basic actions

--- Isolines/Isolines/mainwindow.cpp
+++ Isolines/Isolines/mainwindow.cpp
@@ -1,11 +1,47 @@
 #include "mainwindow.h"
 
+#include <QToolBar>
+#include <QMenu>
+#include <QMenuBar>
+#include <QStatusBar>
+
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
 {
+    this->setupActions();
 }
 
 MainWindow::~MainWindow()
 {
 
+}
+
+void MainWindow::setupActions() {
+    this->statusBar()->show();
+    QToolBar *toolbar = this->addToolBar(tr("Main toolbar"));
+    QMenu *file_menu = this->menuBar()->addMenu(tr("&File"));
+    toolbar->addAction(file_menu->addAction(tr("&New"), [] {
+
+    }, QKeySequence(QString("Ctrl+N"))));
+    toolbar->addAction(file_menu->addAction(tr("&Open"), [] {
+
+    }, QKeySequence(QString("Ctrl+O"))));
+    file_menu->addSeparator();
+    file_menu->addAction(tr("&Quit..."), this, &MainWindow::close, QKeySequence(QString("Ctrl+Q")));
+
+    QMenu *view_menu = this->menuBar()->addMenu(tr("&View"));
+    this->toolbar_switch = view_menu->addAction(tr("&Toolbar"), this, [this, toolbar] {
+        bool b = toolbar->isHidden();
+        toolbar->setHidden(!b);
+        this->toolbar_switch->setChecked(b);
+    });
+    this->toolbar_switch->setCheckable(true);
+    this->toolbar_switch->setChecked(true);
+    this->statusbar_switch = view_menu->addAction(tr("&Statusbar"), this, [this] {
+        bool b = this->statusBar()->isHidden();
+        this->statusBar()->setHidden(!b);
+        this->statusbar_switch->setChecked(b);
+    });
+    this->statusbar_switch->setCheckable(true);
+    this->statusbar_switch->setChecked(true);
 }
--- Isolines/Isolines/mainwindow.h
+++ Isolines/Isolines/mainwindow.h
@@ -2,6 +2,7 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QAction>
 
 class MainWindow : public QMainWindow
 {
@@ -10,6 +11,12 @@ class MainWindow : public QMainWindow
 public:
     MainWindow(QWidget *parent = 0);
     ~MainWindow();
+
+private:
+    void setupActions();
+
+    QAction *toolbar_switch;
+    QAction *statusbar_switch;
 };
 
 #endif // MAINWINDOW_H
