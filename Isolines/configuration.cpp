fb00a1f5bb6fb7e49947497b2913d6f76f4160f6
Alexander Likhachev <likhachev96@gmail.com>
2017-04-06 22:25:18 +0700

    [Isolines] Added new configuration properties

--- Isolines/configuration.cpp
+++ Isolines/configuration.cpp
@@ -3,12 +3,35 @@
 #include <QColor>
 
 Configuration::Configuration(QObject *parent) : QObject(parent),
+    _start_x(0.5),
+    _start_y(0.5),
     _width(100),
     _height(20),
+    _f_min(0),
+    _f_max(0),
+    _cell_width(10),
+    _cell_height(10),
     _levels({qRgb(255, 0, 0), qRgb(255, 83, 0), qRgb(255, 223, 0), qRgb(0, 255, 0), qRgb(0, 184, 217), qRgb(0, 0, 255), qRgb(128, 0, 255)}),
-    _isolines_color(qRgb(255, 255, 255))
+    _isolines_color(qRgb(255, 255, 255)),
+    _interpolate(false)
 {
 
+}
+
+double Configuration::startX() const {
+    return this->_start_x;
+}
+void Configuration::setStartX(double start_x) {
+    this->_start_x = start_x;
+    emit startXChanged(this->_start_x);
+}
+
+double Configuration::startY() const {
+    return this->_start_y;
+}
+void Configuration::setStartY(double start_y) {
+    this->_start_y = start_y;
+    emit startYChanged(this->_start_y);
 }
 
 int Configuration::width() const {
@@ -26,6 +49,52 @@ int Configuration::height() const {
 void Configuration::setHeight(int height) {
     this->_height = height;
     emit heightChanged(this->_height);
+}
+
+int Configuration::cellWidth() const {
+    return this->_cell_width;
+}
+
+void Configuration::setCellWidth(int cell_width) {
+    this->_cell_width = cell_width;
+    emit cellWidthChanged(this->_cell_width);
+}
+
+double Configuration::fMin() const {
+    return this->_f_min;
+}
+
+void Configuration::setFMin(double min) {
+    this->_f_min = min;
+    emit fMinChanged(this->_f_min);
+}
+
+double Configuration::fMax() const {
+    return this->_f_max;
+}
+
+void Configuration::setFMax(double max) {
+    this->_f_max = max;
+    emit fMaxChanged(this->_f_max);
+}
+
+double Configuration::fStep() const {
+    return this->_f_step;
+}
+
+void Configuration::setFStep(double step) {
+    this->_f_step = step;
+    emit fStepChanged(this->_f_step);
+}
+
+
+int Configuration::cellHeight() const {
+    return this->_cell_height;
+}
+
+void Configuration::setCellHeight(int cell_height) {
+    this->_cell_height = cell_height;
+    emit cellHeightChanged(this->_cell_height);
 }
 
 std::vector<QRgb> Configuration::levels() const {
--- Isolines/configuration.h
+++ Isolines/configuration.h
@@ -8,17 +8,38 @@
 class Configuration : public QObject
 {
     Q_OBJECT
+    Q_PROPERTY(double startX READ startX WRITE setStartX NOTIFY startXChanged)
+    Q_PROPERTY(double startY READ startY WRITE setStartY NOTIFY startYChanged)
     Q_PROPERTY(int width READ width WRITE setWidth NOTIFY widthChanged)
     Q_PROPERTY(int height READ height WRITE setHeight NOTIFY heightChanged)
+    Q_PROPERTY(double fMin READ fMin WRITE setFMin NOTIFY fMinChanged)
+    Q_PROPERTY(double fMax READ fMax WRITE setFMax NOTIFY fMaxChanged)
+    Q_PROPERTY(double fStep READ fStep WRITE setFStep NOTIFY fStepChanged)
+    Q_PROPERTY(int cellWidth READ cellWidth WRITE setCellWidth NOTIFY cellWidthChanged)
+    Q_PROPERTY(int cellHeight READ cellHeight WRITE setCellHeight NOTIFY cellHeightChanged)
     Q_PROPERTY(std::vector<QRgb> levels READ levels WRITE setLevels NOTIFY levelsChanged)
     Q_PROPERTY(QRgb isolinesColor READ isolinesColor WRITE setIsolinesColor NOTIFY isolinesColorChanged)
     Q_PROPERTY(bool interpolate READ interpolate WRITE setInterpolate NOTIFY interpolateChanged)
 public:
     explicit Configuration(QObject *parent = 0);
+    double startX() const;
+    void setStartX(double);
+    double startY() const;
+    void setStartY(double);
     int width() const;
     void setWidth(int);
     int height() const;
     void setHeight(int);
+    int cellWidth() const;
+    void setFMin(double);
+    double fMin() const;
+    void setFMax(double);
+    double fMax() const;
+    void setFStep(double);
+    double fStep() const;
+    void setCellWidth(int);
+    int cellHeight() const;
+    void setCellHeight(int);
     std::vector<QRgb> levels() const;
     void setLevels(std::vector<QRgb> &);
     QRgb isolinesColor() const;
@@ -28,18 +49,33 @@ public:
     bool load(QTextStream &);
 
 signals:
+    void startXChanged(double);
+    void startYChanged(double);
     void widthChanged(int);
     void heightChanged(int);
+    void fMinChanged(double);
+    void fMaxChanged(double);
+    void fStepChanged(double);
+    void cellWidthChanged(int);
+    void cellHeightChanged(int);
     void levelsChanged(const std::vector<QRgb> &);
     void isolinesColorChanged(QRgb);
     void interpolateChanged(bool);
 
 private:
+    double _start_x;
+    double _start_y;
     int _width;
     int _height;
+    double _f_min;
+    double _f_max;
+    double _f_step;
+    int _cell_width;
+    int _cell_height;
     std::vector<QRgb> _levels;
     QRgb _isolines_color;
     bool _interpolate;
 };
 
 #endif // CONFIGURATION_H
+
