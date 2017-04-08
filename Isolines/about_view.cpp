1f8911b284a8c54c8b93fb986602d3af78aa1411
Alexander Likhachev <likhachev96@gmail.com>
2017-04-09 01:41:34 +0700

    [Isolines] Updated information about application

--- Isolines/about_view.cpp
+++ Isolines/about_view.cpp
@@ -1,6 +1,6 @@
 #include <QString>
 #include <QPixmap>
-#include <QHBoxLayout>
+#include <QGridLayout>
 #include <QLabel>
 
 #include "about_view.h"
@@ -9,12 +9,16 @@ AboutView::AboutView(QWidget *parent) : QDialog(parent)
 {
     setMinimumSize(350, 200);
     setMaximumSize(350, 200);
-    QHBoxLayout *layout = new QHBoxLayout(this);
+    QGridLayout *layout = new QGridLayout(this);
     QLabel *photo = new QLabel(this);
     QPixmap pixmap = QPixmap(":/photos/author.jpg");
     photo->setPixmap(pixmap);
-    layout->addWidget(photo);
-    QLabel *description = new QLabel(tr("Isolines\n\nАвтор: Александр Лихачев\nФИТ НГУ\nГруппа 14202"), this);
-    layout->addWidget(description);
-    setLayout(layout);
+    QLabel *about = new QLabel(QString("Isolines\n\nAuthor: Alexander Likhachev\nFIT NSU\nGroup 14202"), this);
+    QLabel *desc = new QLabel(tr("Use <b>left mouse button</b> to define extra isoline<br/>"
+                                 "Use <b>right mouse button</b> to delete it<br/>"
+                                 "(in isolines showing mode)"), this);
+    desc->setWordWrap(true);
+    layout->addWidget(photo, 0, 0, 3, 2);
+    layout->addWidget(about, 0, 2);
+    layout->addWidget(desc, 2, 2);
 }
