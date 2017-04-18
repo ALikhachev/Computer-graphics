93f86aa69dc1a32746585c8c4dcf55b7fb1f5724
Alexander Likhachev <likhachev96@gmail.com>
2017-04-18 18:41:14 +0700

    [Wireframe] Basic main window

--- Wireframe/Wireframe.pro
+++ Wireframe/Wireframe.pro
@@ -24,6 +24,11 @@ DEFINES += QT_DEPRECATED_WARNINGS
 
 
 SOURCES += main.cpp\
-        mainwindow.cpp
+        mainwindow.cpp \
+        about_view.cpp
 
-HEADERS  += mainwindow.h
+HEADERS  += mainwindow.h \
+            about_view.h
+
+RESOURCES += \
+    images.qrc
--- Wireframe/mainwindow.h
+++ Wireframe/mainwindow.h
@@ -2,6 +2,8 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QAction>
+#include <QToolBar>
 
 class MainWindow : public QMainWindow
 {
@@ -10,6 +12,31 @@ class MainWindow : public QMainWindow
 public:
     MainWindow(QWidget *parent = 0);
     ~MainWindow();
+
+private slots:
+    void selectAndOpenScene();
+    void selectAndSaveScene();
+    void openRecentFile();
+    void showAbout();
+    void showConfiguration();
+
+private:
+    void initActions();
+    void initFileMenu(QToolBar *toolbar);
+    void initViewMenu(QToolBar *toolbar);
+    void showError(const QString &text) const;
+    void openScene(const QString &filename);
+    void updateScene();
+    void saveScene(const QString &filename);
+    void setCurrentFile(const QString &filename);
+    void updateRecentFileActions();
+    static QString strippedName(const QString &full_filename);
+
+    const static int MaxRecentFiles = 5;
+    QAction *separator_action;
+    QAction *recent_file_acts[MaxRecentFiles];
+    QAction *toolbar_switch;
+    QAction *statusbar_switch;
 };
 
 #endif // MAINWINDOW_H
